4d088081ee7402375df779b8ed9d73a043a58f89
yatsubashi <37011432+yatsubashi@users.noreply.github.com>
2020-04-13 07:29:46 +0900

    Tutorial: Chapter 3

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,9 @@
 import React from "react"
-import Header from "../components/header"
+import Layout from "../components/layout"
 
 export default () => (
-	<div style={{ color: `teal` }}>
-		<Header headerText="About Gatsby" />
-		<p>Such wow. Very React.</p>
-	</div>
+	<Layout>
+		<h1>About me</h1>
+		<p>I’m good enough, I’m smart enough, and gosh darn it, people like me!</p>
+	</Layout>
 )
